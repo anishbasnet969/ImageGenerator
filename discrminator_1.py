413a22faebe59548fe3363446ed50ed2686f21db
sauravniraula <sauravniraula912@gmail.com>
2023-01-05 16:58:52 +0545

    channel dimension in float error

--- discrminator_1.py
+++ discrminator_1.py
@@ -16,11 +16,11 @@ class StageIDiscriminator(nn.Module):
         self.compress = nn.Linear(tem_size, Nd)
 
         # 1x1 conv for channel adjustment
-        self.channel_resize = nn.Conv2d(64 * 8 + Nd, (64 * 8 + Nd) / 2, 1, 1, 0)
+        self.channel_resize = nn.Conv2d(64 * 8 + Nd, int((64 * 8 + Nd) / 2), 1, 1, 0)
 
         self.flatten = nn.Flatten()
 
-        self.classifier = nn.Linear(((64 * 8 + Nd) / 2) * 4 * 4, 1)
+        self.classifier = nn.Linear(int((64 * 8 + Nd) / 2) * 4 * 4, 1)
 
     def downsampling_block(
         self, in_channels, out_channels, kernel_size, stride, padding
@@ -38,16 +38,19 @@ class StageIDiscriminator(nn.Module):
             nn.LeakyReLU(0.1),
         )
 
-    def forward(self, img, tem):
-        x = self.down_sampler(img)
-        compressed_em = self.compress(tem)
-        em_to_fm = compressed_em.resize(
-            compressed_em.shape[0], compressed_em.shape[1], 1, 1
-        )
-        replicated_fm = em_to_fm.repeat(1, 1, 4, 4)
-        concatenated_fm = torch.cat(x, replicated_fm, dim=1)
-        text_img_fm = self.channel_resize(concatenated_fm)
-        flattened_vec = self.flatten(text_img_fm)
-        score = self.classifier(flattened_vec)
-        output = F.sigmoid(score)
-        return output
+    # def forward(self, img, tem):
+    #     x = self.down_sampler(img)
+    #     compressed_em = self.compress(tem)
+    #     em_to_fm = compressed_em.resize(
+    #         compressed_em.shape[0], compressed_em.shape[1], 1, 1
+    #     )
+    #     replicated_fm = em_to_fm.repeat(1, 1, 4, 4)
+    #     concatenated_fm = torch.cat(x, replicated_fm, dim=1)
+    #     text_img_fm = self.channel_resize(concatenated_fm)
+    #     flattened_vec = self.flatten(text_img_fm)
+    #     score = self.classifier(flattened_vec)
+    #     output = F.sigmoid(score)
+    #     return output
+
+if __name__ == "__main__":
+    print(StageIDiscriminator(300, 128))
