3642339edd610f634a851777b103bd2d80801a45
Anish Basnet <anishbasnet969@gmail.com>
2023-01-05 16:35:42 +0545

    Stage I Discriminator Module

---
+++ discrminator_1.py
@@ -0,0 +1,53 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class StageIDiscriminator(nn.Module):
+    def __init__(self, tem_size, Nd):
+        super(StageIDiscriminator, self).__init__()
+        self.down_sampler = nn.Sequential(
+            nn.Conv2d(3, 64, 4, 2, 1),
+            nn.LeakyReLU(0.1),
+            self.downsampling_block(64, 64 * 2, 4, 2, 1),
+            self.downsampling_block(64 * 2, 64 * 4, 4, 2, 1),
+            self.downsampling_block(64 * 4, 64 * 8, 4, 2, 1),
+        )
+        self.compress = nn.Linear(tem_size, Nd)
+
+        # 1x1 conv for channel adjustment
+        self.channel_resize = nn.Conv2d(64 * 8 + Nd, (64 * 8 + Nd) / 2, 1, 1, 0)
+
+        self.flatten = nn.Flatten()
+
+        self.classifier = nn.Linear(((64 * 8 + Nd) / 2) * 4 * 4, 1)
+
+    def downsampling_block(
+        self, in_channels, out_channels, kernel_size, stride, padding
+    ):
+        return nn.Sequential(
+            nn.Conv2d(
+                in_channels,
+                out_channels,
+                kernel_size,
+                stride,
+                padding,
+                bias=False,
+            ),
+            nn.BatchNorm2d(out_channels),
+            nn.LeakyReLU(0.1),
+        )
+
+    def forward(self, img, tem):
+        x = self.down_sampler(x)
+        compressed_em = self.compress(tem)
+        em_to_fm = compressed_em.resize(
+            compressed_em.shape[0], compressed_em.shape[1], 1, 1
+        )
+        replicated_fm = em_to_fm.repeat(1, 1, 4, 4)
+        concatenated_fm = torch.cat(x, replicated_fm, dim=1)
+        text_img_fm = self.channel_resize(concatenated_fm)
+        flattened_vec = self.flatten(text_img_fm)
+        score = self.classifier(flattened_vec)
+        output = F.sigmoid(score)
+        return output
--- generator_1.py
+++ generator_1.py
@@ -1,29 +1,45 @@
 import torch
-from torch import nn
+import torch.nn as nn
+
 
 class StageIGenerator(nn.Module):
-  def __init__(self, con_a_size, latent_size):
-    super(StageIGenerator, self).__init__();
+    def __init__(self, con_a_size, latent_size):
+        super(StageIGenerator, self).__init__()
 
-    self.upsampling = nn.Sequential(
-      nn.ConvTranspose2d(con_a_size+latent_size, 196, 4, 1, 0),
-      nn.ReLU(),
-      nn.ConvTranspose2d(196, 128, 4, 2, 1),
-      nn.ReLU(),
-      nn.ConvTranspose2d(128, 64, 4, 2, 1),
-      nn.ReLU(),
-      nn.ConvTranspose2d(64, 24, 4, 2, 1),
-      nn.ReLU(),
-      nn.ConvTranspose2d(24, 12, 4, 2, 1),
-      nn.ReLU(),
-      nn.Conv2d(12, 3, 1)
-    )
+        self.upsampling = nn.Sequential(
+            self.upsampling_block(
+                con_a_size + latent_size,
+                196,
+                4,
+                1,
+                0,
+            ),
+            self.upsampling_block(196, 128, 4, 2, 1),
+            self.upsampling_block(128, 64, 4, 2, 1),
+            self.upsampling_block(64, 24, 4, 2, 1),
+            self.upsampling_block(24, 12, 4, 2, 1),
+            nn.Conv2d(12, 3, 1),
+        )
 
-  def forward(self, x):
-    x = x.reshape(x.shape[0], x.shape[1], 1, 1)
-    return self.upsampling(x)
+    def upsampling_block(self, in_channels, out_channels, kernel_size, stride, padding):
+        return nn.Sequential(
+            nn.ConvTranspose2d(
+                in_channels,
+                out_channels,
+                kernel_size,
+                stride,
+                padding,
+                bias=False,
+            ),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(),
+        )
+
+    def forward(self, x):
+        x = x.reshape(x.shape[0], x.shape[1], 1, 1)
+        return self.upsampling(x)
 
 
 if __name__ == "__main__":
-  generator = StageIGenerator(128, 100)
-  print(generator(torch.randn(8, 228)).shape)
+    generator = StageIGenerator(128, 100)
+    print(generator(torch.randn(8, 228)).shape)
