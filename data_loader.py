753e29d6c6d05df2218449ad77b8a6a12fa03295
Anish Basnet <anishbasnet969@gmail.com>
2023-05-17 06:13:14 +0545

    batch shapes

--- data_loader.py
+++ data_loader.py
@@ -72,7 +72,6 @@ class Collate:
         )
         imgs = [item[1].unsqueeze(0) for item in batch]
         imgs = torch.cat(imgs, dim=0)
-        print(imgs.shape)
         return tokenized_texts, imgs
 
 
