7dc7f43e6ddd9acf1b1bb92e45cb3d5c2c953a7e
Anish Basnet <anishbasnet969@gmail.com>
2023-05-20 16:58:31 +0545

    vision utilities

--- v2_implementation/generate.py
+++ v2_implementation/generate.py
@@ -29,6 +29,8 @@ ImageFile.LOAD_TRUNCATED_IMAGES = True
 from subprocess import Popen, PIPE
 import re
 
+from vision_utils import random_noise_image, random_gradient_image, resample
+
 import warnings
 
 warnings.filterwarnings("ignore")
@@ -106,15 +108,6 @@ parser.add_argument(
     dest="step_size",
 )
 parser.add_argument(
-    "-cutm",
-    "--cut_method",
-    type=str,
-    help="Cut method",
-    choices=["original", "updated", "nrupdated", "updatedpooling", "latest"],
-    default="latest",
-    dest="cut_method",
-)
-parser.add_argument(
     "-cuts", "--num_cuts", type=int, help="Number of cuts", default=32, dest="cutn"
 )
 parser.add_argument(
@@ -127,17 +120,6 @@ parser.add_argument(
     action="store_true",
     help="Enable cudnn.deterministic?",
     dest="cudnn_determinism",
-)
-parser.add_argument(
-    "-aug",
-    "--augments",
-    nargs="+",
-    action="append",
-    type=str,
-    choices=["Ji", "Sh", "Gn", "Pe", "Ro", "Af", "Et", "Ts", "Cr", "Er", "Re"],
-    help="Enabled augments (latest vut method only)",
-    default=[],
-    dest="augments",
 )
 parser.add_argument(
     "-cd",
@@ -166,89 +148,6 @@ if not args.augments:
 
 if not args.cuda_device == "cpu" and not torch.cuda.is_available():
     args.cuda_device = "cpu"
-
-
-def sinc(x):
-    return torch.where(x != 0, torch.sin(math.pi * x) / (math.pi * x), x.new_ones([]))
-
-
-def lanczos(x, a):
-    cond = torch.logical_and(-a < x, x < a)
-    out = torch.where(cond, sinc(x) * sinc(x / a), x.new_zeros([]))
-    return out / out.sum()
-
-
-def ramp(ratio, width):
-    n = math.ceil(width / ratio + 1)
-    out = torch.empty([n])
-    cur = 0
-    for i in range(out.shape[0]):
-        out[i] = cur
-        cur += ratio
-    return torch.cat([-out[1:].flip([0]), out])[1:-1]
-
-
-def random_noise_image(w, h):
-    random_image = Image.fromarray(
-        np.random.randint(0, 255, (w, h, 3), dtype=np.dtype("uint8"))
-    )
-    return random_image
-
-
-def gradient_2d(start, stop, width, height, is_horizontal):
-    if is_horizontal:
-        return np.tile(np.linspace(start, stop, width), (height, 1))
-    else:
-        return np.tile(np.linspace(start, stop, height), (width, 1)).T
-
-
-def gradient_3d(width, height, start_list, stop_list, is_horizontal_list):
-    result = np.zeros((height, width, len(start_list)), dtype=float)
-
-    for i, (start, stop, is_horizontal) in enumerate(
-        zip(start_list, stop_list, is_horizontal_list)
-    ):
-        result[:, :, i] = gradient_2d(start, stop, width, height, is_horizontal)
-
-    return result
-
-
-def random_gradient_image(w, h):
-    array = gradient_3d(
-        w,
-        h,
-        (0, 0, np.random.randint(0, 255)),
-        (
-            np.random.randint(1, 255),
-            np.random.randint(2, 255),
-            np.random.randint(3, 128),
-        ),
-        (True, False, False),
-    )
-    random_image = Image.fromarray(np.uint8(array))
-    return random_image
-
-
-def resample(input, size, align_corners=True):
-    n, c, h, w = input.shape
-    dh, dw = size
-
-    input = input.view([n * c, 1, h, w])
-
-    if dh < h:
-        kernel_h = lanczos(ramp(dh / h, 2), 2).to(input.device, input.dtype)
-        pad_h = (kernel_h.shape[0] - 1) // 2
-        input = F.pad(input, (0, 0, pad_h, pad_h), "reflect")
-        input = F.conv2d(input, kernel_h[None, None, :, None])
-
-    if dw < w:
-        kernel_w = lanczos(ramp(dw / w, 2), 2).to(input.device, input.dtype)
-        pad_w = (kernel_w.shape[0] - 1) // 2
-        input = F.pad(input, (pad_w, pad_w, 0, 0), "reflect")
-        input = F.conv2d(input, kernel_w[None, None, None, :])
-
-    input = input.view([n, c, h, w])
-    return F.interpolate(input, size, mode="bicubic", align_corners=align_corners)
 
 
 class ReplaceGrad(torch.autograd.Function):
@@ -326,92 +225,30 @@ class MakeCutouts(nn.Module):
         super().__init__()
         self.cut_size = cut_size
         self.cutn = cutn
-        self.cut_pow = cut_pow  # not used with pooling
-
-        # Pick your own augments & their order
-        augment_list = []
-        for item in args.augments[0]:
-            if item == "Ji":
-                augment_list.append(
-                    K.ColorJitter(
-                        brightness=0.1, contrast=0.1, saturation=0.1, hue=0.1, p=0.7
-                    )
-                )
-            elif item == "Sh":
-                augment_list.append(K.RandomSharpness(sharpness=0.3, p=0.5))
-            elif item == "Gn":
-                augment_list.append(K.RandomGaussianNoise(mean=0.0, std=1.0, p=0.5))
-            elif item == "Pe":
-                augment_list.append(K.RandomPerspective(distortion_scale=0.7, p=0.7))
-            elif item == "Ro":
-                augment_list.append(K.RandomRotation(degrees=15, p=0.7))
-            elif item == "Af":
-                augment_list.append(
-                    K.RandomAffine(
-                        degrees=15,
-                        translate=0.1,
-                        shear=5,
-                        p=0.7,
-                        padding_mode="zeros",
-                        keepdim=True,
-                    )
-                )  # border, reflection, zeros
-            elif item == "Et":
-                augment_list.append(K.RandomElasticTransform(p=0.7))
-            elif item == "Ts":
-                augment_list.append(
-                    K.RandomThinPlateSpline(scale=0.8, same_on_batch=True, p=0.7)
-                )
-            elif item == "Cr":
-                augment_list.append(
-                    K.RandomCrop(
-                        size=(self.cut_size, self.cut_size),
-                        pad_if_needed=True,
-                        padding_mode="reflect",
-                        p=0.5,
-                    )
-                )
-            elif item == "Er":
-                augment_list.append(
-                    K.RandomErasing(
-                        scale=(0.1, 0.4),
-                        ratio=(0.3, 1 / 0.3),
-                        same_on_batch=True,
-                        p=0.7,
-                    )
-                )
-            elif item == "Re":
-                augment_list.append(
-                    K.RandomResizedCrop(
-                        size=(self.cut_size, self.cut_size),
-                        scale=(0.1, 1),
-                        ratio=(0.75, 1.333),
-                        cropping_mode="resample",
-                        p=0.5,
-                    )
-                )
-
-        self.augs = nn.Sequential(*augment_list)
+        self.cut_pow = cut_pow
+        self.augs = nn.Sequential(
+            K.RandomHorizontalFlip(p=0.5),
+            K.ColorJitter(hue=0.01, saturation=0.01, p=0.7),
+            K.RandomSharpness(0.3, p=0.4),
+            K.RandomAffine(degrees=30, translate=0.1, p=0.8, padding_mode="border"),
+            K.RandomPerspective(0.2, p=0.4),
+        )
         self.noise_fac = 0.1
-        # self.noise_fac = False
-
-        # Uncomment if you like seeing the list ;)
-        # print(augment_list)
-
-        # Pooling
-        self.av_pool = nn.AdaptiveAvgPool2d((self.cut_size, self.cut_size))
-        self.max_pool = nn.AdaptiveMaxPool2d((self.cut_size, self.cut_size))
 
     def forward(self, input):
+        sideY, sideX = input.shape[2:4]
+        max_size = min(sideX, sideY)
+        min_size = min(sideX, sideY, self.cut_size)
         cutouts = []
-
         for _ in range(self.cutn):
-            # Use Pooling
-            cutout = (self.av_pool(input) + self.max_pool(input)) / 2
-            cutouts.append(cutout)
-
+            size = int(
+                torch.rand([]) ** self.cut_pow * (max_size - min_size) + min_size
+            )
+            offsetx = torch.randint(0, sideX - size + 1, ())
+            offsety = torch.randint(0, sideY - size + 1, ())
+            cutout = input[:, :, offsety : offsety + size, offsetx : offsetx + size]
+            cutouts.append(resample(cutout, (self.cut_size, self.cut_size)))
         batch = self.augs(torch.cat(cutouts, dim=0))
-
         if self.noise_fac:
             facs = batch.new_empty([self.cutn, 1, 1, 1]).uniform_(0, self.noise_fac)
             batch = batch + facs * torch.randn_like(batch)
@@ -445,8 +282,8 @@ perceptor = (
 cut_size = perceptor.visual.input_resolution
 f = 2 ** (model.decoder.num_resolutions - 1)
 
-if args.cut_method == "latest":
-    make_cutouts = MakeCutouts(cut_size, args.cutn, cut_pow=args.cut_pow)
+
+make_cutouts = MakeCutouts(cut_size, args.cutn, cut_pow=args.cut_pow)
 
 toksX, toksY = args.size[0] // f, args.size[1] // f
 sideX, sideY = toksX * f, toksY * f
@@ -520,7 +357,7 @@ def ascend_txt():
 
     for prompt in pMs:
         result.append(prompt(iii))
-    
+
     return result
 
 
@@ -528,8 +365,7 @@ def train(i):
     opt.zero_grad(set_to_none=True)
     lossAll = ascend_txt()
 
-    if i % args.display_freq == 0:
-        checkin(i, lossAll)
+    checkin(i, lossAll)
 
     loss = sum(lossAll)
     loss.backward()
---
+++ v2_implementation/vision_utils.py
@@ -0,0 +1,88 @@
+import math
+import numpy as np
+import torch
+from torch.nn import functional as F
+from PIL import Image
+
+
+def sinc(x):
+    return torch.where(x != 0, torch.sin(math.pi * x) / (math.pi * x), x.new_ones([]))
+
+
+def lanczos(x, a):
+    cond = torch.logical_and(-a < x, x < a)
+    out = torch.where(cond, sinc(x) * sinc(x / a), x.new_zeros([]))
+    return out / out.sum()
+
+
+def ramp(ratio, width):
+    n = math.ceil(width / ratio + 1)
+    out = torch.empty([n])
+    cur = 0
+    for i in range(out.shape[0]):
+        out[i] = cur
+        cur += ratio
+    return torch.cat([-out[1:].flip([0]), out])[1:-1]
+
+
+def random_noise_image(w, h):
+    random_image = Image.fromarray(
+        np.random.randint(0, 255, (w, h, 3), dtype=np.dtype("uint8"))
+    )
+    return random_image
+
+
+def gradient_2d(start, stop, width, height, is_horizontal):
+    if is_horizontal:
+        return np.tile(np.linspace(start, stop, width), (height, 1))
+    else:
+        return np.tile(np.linspace(start, stop, height), (width, 1)).T
+
+
+def gradient_3d(width, height, start_list, stop_list, is_horizontal_list):
+    result = np.zeros((height, width, len(start_list)), dtype=float)
+
+    for i, (start, stop, is_horizontal) in enumerate(
+        zip(start_list, stop_list, is_horizontal_list)
+    ):
+        result[:, :, i] = gradient_2d(start, stop, width, height, is_horizontal)
+
+    return result
+
+
+def random_gradient_image(w, h):
+    array = gradient_3d(
+        w,
+        h,
+        (0, 0, np.random.randint(0, 255)),
+        (
+            np.random.randint(1, 255),
+            np.random.randint(2, 255),
+            np.random.randint(3, 128),
+        ),
+        (True, False, False),
+    )
+    random_image = Image.fromarray(np.uint8(array))
+    return random_image
+
+
+def resample(input, size, align_corners=True):
+    n, c, h, w = input.shape
+    dh, dw = size
+
+    input = input.view([n * c, 1, h, w])
+
+    if dh < h:
+        kernel_h = lanczos(ramp(dh / h, 2), 2).to(input.device, input.dtype)
+        pad_h = (kernel_h.shape[0] - 1) // 2
+        input = F.pad(input, (0, 0, pad_h, pad_h), "reflect")
+        input = F.conv2d(input, kernel_h[None, None, :, None])
+
+    if dw < w:
+        kernel_w = lanczos(ramp(dw / w, 2), 2).to(input.device, input.dtype)
+        pad_w = (kernel_w.shape[0] - 1) // 2
+        input = F.pad(input, (pad_w, pad_w, 0, 0), "reflect")
+        input = F.conv2d(input, kernel_w[None, None, None, :])
+
+    input = input.view([n, c, h, w])
+    return F.interpolate(input, size, mode="bicubic", align_corners=align_corners)
